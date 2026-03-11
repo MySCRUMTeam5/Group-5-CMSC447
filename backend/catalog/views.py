d6205a5cd206e11c44ce50ffe8bf5e4d4a3cef22
Dennette Affinito <daffini1@umbc.edu>
2026-03-11 06:01:22 -0400

    Started adding filter, updated sort

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,47 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
-def sort_filter_collection(request):
-    #DO WE WANT ANY OTHER SORT FEATURES?
-    price_ascend = False
-    price_descend = False
-
-    newest_item = False
-    oldest_item = False
-
-    alpha_ascend = False
-    alpha_descend = False
-
+@require_http_methods(["GET"])
+def sort_filter_collection(request):  
     sort = request.GET.get("sort")
     
     filter_val = request.GET.get("filter")
+
+    #DO WE WANT ANY OTHER SORT FEATURES?
+    #Names may need to be changed based on how we store our data in the database
+    sort_values = {
+        "p_price_ascend" : "purchase_price",
+        "p_price_descend" : "-purchase_price",
+        "alpha_ascend" : "alpha",
+        "alpha_descend" : "-alpha",
+        "date_ascend" : "purchase_date",
+        "date_descend" : "-purchase_date"
+    }
+
+    filter_values = {
+        #add in filter values here
+    }
+
+    sort_by = sort_values.get(sort)
 
     con = sqlite3.connect("collection.db") #may need to change this name later
 
     cur = con.cursor()
 
-    #THIS IS PURE PYTHON RIGHT NOW, FIGURE OUT JS FRONT END CRAP LATER
-    if sort:
-        #find sort field and mark as a bool true
-        if (price_ascend == True): 
-            for row in cur.execute('SELECT * FROM collection ORDER BY price DESC;'): 
-                print(row)
-        elif (price_descend == True):
-            for row in cur.execute('SELECT * FROM collection ORDER BY price ASC')
-                print(row)
-
-        elif (newest_item == True):
-            for row in cur.execute('SELECT * FROM collection ORDER BY date DESC;'): 
-                print(row) 
-        elif (oldest_item == True):
-            for row in cur.execute('SELECT * FROM collection ORDER BY date ASC'):
-                print(row)
-        
-        elif (alpha_descend == True):
-            for row in cur.execute('SELECT * FROM collection ORDER BY name DESC;'): 
-                print(row) 
-            for row in cur.execute('SELECT * FROM collection ORDER BY name ASC'):
-                print(row)
+    if sort and sort_by:
+        data_sorted = Collection.objects.all().order_by(sort_by).values()
     
-    elif filter_val:
-        #find all the values from filter and display
+    elif filter_val and filter_by:
+        #fill in parameter for filter function here
+        data_filtered = Collection.objects.filter().values()
+    
+    elif not sort_by or not filter_by:
+        return JsonResponse({"error": "this action could not be completed", status = 400})
