1527569a722b25407ea3bba2c38ff402b20589b3
aayala25 <abelayala1441@gmail.com>
2026-03-11 09:33:56 -0400

    updated to handle deleting from different colections

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("items/delete/<int:item_id>/", views.delete_item, name="delete_item"),
+urlpatterns = [path("items/delete/<int:collection_id>/<int:item_id>/", views.delete_item, name="delete_item"),
 ]
--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -3,12 +3,12 @@ from django.http import JsonResponse
 from backend.catalog.models import Item
 
 # Create your views here.
-def delete_item(request, item_id):
+def delete_item(request, collection_id, item_id):
     try:
-        item = Item.objects.get(id=item_id)
+        item = Item.objects.get(id=item_id, collection_id=collection_id)
         item_name = item.name
         item.delete()        
-        return JsonResponse({"message": f"Item: '{item_name}' deleted successfully."})
+        return JsonResponse({"message": f"Item: '{item_name}' deleted successfully!"})
     except Item.DoesNotExist:
-        return JsonResponse({"error": "Item not found."}, status=404)
+        return JsonResponse({"error": "Item not deleted. Item not found in this collection!"}, status=404)
         
