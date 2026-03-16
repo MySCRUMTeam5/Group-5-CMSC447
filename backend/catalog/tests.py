c63e3a64f6deb86b0f282888e591d777b697e02c
AhadSamee17 <asamee1@umbc.edu>
2026-03-16 03:35:58 -0400

    Implemented test cases for add item

--- backend/catalog/tests.py
+++ backend/catalog/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from .models import User, Collection, Item
 
-# Create your tests here.
+
+class AddItemTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="test123")
+        self.collection = Collection.objects.create(owner=self.user, name="Pokemon Cards")
+
+    def test_cant_add_empty_item(self):
+        response = self.client.post("/api/items/add/",
+            data=json.dumps({}),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_add_without_collection_id(self):
+        response = self.client.post("/api/items/add/",
+            data=json.dumps({
+                "name": "Charizard"
+            }),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_add_without_name(self):
+        response = self.client.post("/api/items/add/",
+            data=json.dumps({
+                "collection_id": self.collection.id
+            }),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_cant_add_without_collection_id_and_name(self):
+        response = self.client.post("/api/items/add/",
+            data=json.dumps({
+                "category": "cards"
+            }),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 400)
